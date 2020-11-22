deb356d55e557c4973693ec9b1ad8342c3f3aaf9
Guido Perucchini <guido.perucchini@gmail.com>
2020-11-22 13:10:37 +0100

    Initial Commit

---
+++ magicapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  magicapp
+//
+//  Created by Guido Perucchini on 22/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ magicapp/magicappApp.swift
@@ -0,0 +1,17 @@
+//
+//  magicappApp.swift
+//  magicapp
+//
+//  Created by Guido Perucchini on 22/11/20.
+//
+
+import SwiftUI
+
+@main
+struct magicappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
